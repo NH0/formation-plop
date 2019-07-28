e5f5b78b898063d2420bf371d6ba325d337957cc
NH0 <85ca4663@opayq.com>
2019-07-28 19:37:29 +0200

    Create react app and design it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
+import {BrowserRouter } from 'react-router-dom';
+import { Routes } from './routes';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <img className="Logo" src={logo} alt="logo"/>
+        <div className="Text">
+          Easy recipes
+          <div className="Subtitle">Learn to cook with delicious and simple recipes</div>
+        </div>
+      </div>
+      <BrowserRouter>
+      <div className="App-body">
+        <div className="Menu">
+          Recipes
+        </div>
+        <div className="Recipe">
+            <Routes />
+        </div>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
