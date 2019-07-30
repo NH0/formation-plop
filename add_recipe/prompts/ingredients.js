d555d60d27ec6efe6e12780f4bc42ce7c7143a0a
NH0 <85ca4663@opayq.com>
2019-07-31 00:51:56 +0200

    Step 4 : List of ingredients

--- add_recipe/prompts/ingredients.js
+++ add_recipe/prompts/ingredients.js
@@ -1,19 +1,38 @@
 const getOneIngredient = () => [
-    {
-      type: "input",
-      name: "ingredient",
-      message: "Quantity and name of ingredient :"
-    }
-  ];
+  {
+    type: "list",
+    name: "ingredient",
+    message: "Quantity and name of ingredient :",
+    choices: [
+      "Eggs",
+      "Flour",
+      "Rice",
+      "Pasta",
+      "Potatoes",
+      "Salt and pepper",
+      "Sugar",
+      "Butter",
+      "Milk",
+      "Olive oil",
+      "Vinegar",
+      "Ground meat",
+      "Tomatoes",
+      "Basil",
+      "Carrots",
+      "Bay leafs",
+      "Thyme"
+    ]
+  }
+];
 
 const getIngredients = () => ({
-    type: "recursive",
-    name: "ingredients",
-    message: "Would you like to add an ingredient ?",
-    default: "y",
-    prompts: getOneIngredient()
-  });
+  type: "recursive",
+  name: "ingredients",
+  message: "Would you like to add an ingredient ?",
+  default: "y",
+  prompts: getOneIngredient()
+});
 
 module.exports = {
-    getIngredients,
+  getIngredients
 };
