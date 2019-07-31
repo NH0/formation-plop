642fbed45db5722e280333ad0dce6f1e43b3cf94
Thibault F <thibaultf@sipios.com>
2019-07-31 14:05:59 +0200

    Step 5 : Choose the units with when and filter

--- add_recipe/prompts/ingredients.js
+++ add_recipe/prompts/ingredients.js
@@ -1,37 +1,84 @@
 const getOneIngredient = () => [
-    {
-      type: "list",
-      name: "ingredient",
-      message: "Quantity and name of ingredient :",
-      choices: ['Oeufs',
-                'Farine',
-                'Riz',
-                'Pâtes',
-                'Pommes de terre',
-                'Sel/Poivre',
-                'Sucre',
-                'Beurre',
-                'Lait',
-                'Huile d\'olive',
-                'Vinaigre',
-                'Viande hachée',
-                'Tomates',
-                'Basilic',
-                'Carrotes',
-                'Laurier',
-                'Thym',
-            ]
+  {
+    type: "list",
+    name: "ingredient",
+    message: "Name of ingredient :",
+    choices: [
+      "Eggs",
+      "Flour",
+      "Rice",
+      "Pasta",
+      "Potatoes",
+      "Salt and pepper",
+      "Sugar",
+      "Butter",
+      "Milk",
+      "Olive oil",
+      "Vinegar",
+      "Meat",
+      "Tomatoes",
+      "Basil",
+      "Carrots",
+      "Laurier",
+      "Thyme"
+    ]
+  },
+  {
+    type: "list",
+    name: "unit",
+    message: function(answers) {
+      return "Unit of " + answers.ingredient;
+    },
+    choices: ["L", "cL", "mL", "teaspoon", "tablespoon", "glass"],
+    when: function(answers) {
+      return ["Milk", "Olive oil", "Vinegar"].includes(answers.ingredient);
     }
-  ];
+  },
+  {
+    type: "list",
+    name: "unit",
+    message: function(answers) {
+      return "Unit of " + answers.ingredient;
+    },
+    choices: ["kg", "g", "teaspoon", "tablespoon", "No unit"],
+    when: function(answers) {
+      return [
+        "Flour",
+        "Rice",
+        "Pasta",
+        "Potatoes",
+        "Salt and pepper",
+        "Sugar",
+        "Butter",
+        "Meat",
+        "Tomatoes",
+        "Carrots"
+      ].includes(answers.ingredient);
+    }
+  },
+  {
+    type: "input",
+    name: "quantity",
+    message: function(answers) {
+      return "Quantity of " + answers.ingredient;
+    },
+    validate: function(quantity) {
+      if (isNaN(quantity.trim())) {
+        return "You must provide a number !";
+      }
+      return true;
+    }
+  }
+];
 
 const getIngredients = () => ({
-    type: "recursive",
-    name: "ingredients",
-    message: "Would you like to add an ingredient ?",
-    default: "y",
-    prompts: getOneIngredient()
-  });
+  type: "recursive",
+  name: "ingredients",
+  message: "Would you like to add an ingredient ?",
+  default: "y",
+  prompts: getOneIngredient()
+});
 
 module.exports = {
-    getIngredients,
+  getIngredients
 };
